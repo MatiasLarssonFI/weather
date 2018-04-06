fe0c2bf00948db7f9dcf6989448a2904d88153c3
MatiasLarssonFI <matias.larsson@gmail.com>
2018-04-06 23:55:57 +0300

    Sunny -> Clear

--- src/nordicinterpreter.cxx
+++ src/nordicinterpreter.cxx
@@ -6,9 +6,9 @@
 
 WeatherInterpretation NordicInterpreter::interpret(WeatherRecord const & wr) const {
     const bool is_rainy = wr.rain_vol > (unsigned)1,
-        is_sunny = wr.cloud_percentage < (unsigned)10,
+        is_clear = wr.cloud_percentage < (unsigned)10,
         is_windy = wr.wind_speed > (unsigned)5,
         is_warm = wr.temperature > 12;
 
-    return WeatherInterpretation { is_rainy, is_sunny, is_windy, is_warm };
+    return WeatherInterpretation { is_rainy, is_clear, is_windy, is_warm };
 }
